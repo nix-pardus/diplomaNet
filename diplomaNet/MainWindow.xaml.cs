5e524f67b78648029fe56d474613483370886c86
nix-pardus <pvamotorola@mail.ru>
2022-05-21 01:49:00 +0300

    Добавьте файлы проекта.

---
+++ diplomaNet/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace diplomaNet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void phoneList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            modelTextBox.Text = ((Phone)phoneList.SelectedItems[0]).Title;
+            manufacturerTextBox.Text = ((Phone)phoneList.SelectedItems[0]).Company;
+            priceTextBox.Text = ((Phone)phoneList.SelectedItems[0]).Price.ToString();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((Phone)phoneList.SelectedItems[0]).Title = modelTextBox.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = true;
+        }
+    }
+
+}
---
+++ diplomaNet/XamlConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace diplomaNet
+{
+    public class XamlConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 57;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
