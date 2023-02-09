056f5434b28c7d9af02d97626635f535c6699453
nix-pardus <80983600+nix-pardus@users.noreply.github.com>
2023-02-09 14:55:15 +0300

    before MVVM

---
+++ diplomaNet/View/ListViewHiegthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace diplomaNet.View
+{
+    internal class ListViewHiegthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = double.Parse(value.ToString());
+            double result2 = result - 285;
+            return result2 < 0 ? result : result2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
